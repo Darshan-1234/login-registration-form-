e0ae64688a538bb17762f55245274e2e2330b59a
Darshan Ranpariya <110370435+Darshan-1234@users.noreply.github.com>
2023-04-12 14:30:18 +0530

    Add files via upload

---
+++ Login_Register_Design/src/login_register_design/Login_Register_Design.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login_register_design;
+
+/**
+ *
+ * @author DARSHAN
+ */
+public class Login_Register_Design {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
